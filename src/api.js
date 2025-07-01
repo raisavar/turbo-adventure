55adf1626f1713e9be9fdc5943cffb7e5604bef5
raisavar <raisavar@users.noreply.github.com>
2025-07-01 21:10:31 +0300

    style: improve error handling

--- src/api.js
+++ src/api.js
@@ -266,3 +266,30 @@ class API {
 
 // Update 77
 module.exports = API;
+
+
+// API module for TurboGame
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 79
+module.exports = API;
