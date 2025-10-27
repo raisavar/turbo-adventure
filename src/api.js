f5cd5bd40d3b4fc3e969eb249a21da426c7337ae
raisavar <raisavar@users.noreply.github.com>
2025-10-27 08:37:29 +0200

    style: build configuration update

---
+++ src/api.js
@@ -0,0 +1,25 @@
+// API module for TurboGame
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 4
+module.exports = API;
